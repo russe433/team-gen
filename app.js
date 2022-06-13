5f383f3e325f180c8034c068e2177433bc2b0ba1
russe433 <russe433@msu.edu>
2022-06-13 18:54:54 -0400

    reconfigured app.js page

--- app.js
+++ app.js
@@ -3,75 +3,15 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 const managerInfo = require('./lib/manager');
-const engineerInfo = require('./lib/engineer');
-const internInfo = require('./lib/intern');
+// const engineerInfo = require('./lib/engineer');
+// const internInfo = require('./lib/intern');
 const generateHTML = require('./page/generateHTML');
 
 const teamArr = ['Manager', 'Engineer', 'Intern']
 
-
-async function addManager () {
-    return inquirer.prompt ([
-        {
-            title: 'input',
-            name: 'name',
-            message: "Please enter manager's name.",
-            validate: titleInput => {
-                if (titleInput) {
-                  return true;
-                } else {
-                console.log("Please enter manager's name.");
-                return false;
-                }
-                }
-            },
-            {
-            title: 'input',
-            name: 'id',
-            message: "Please enter manager's id number.",
-            validate: titleInput => {
-                if (titleInput) {
-                  return true;
-                } else {
-                console.log("Please enter manager's id number.");
-                return false;
-                }
-                }
-            },
-            {
-                title: 'input',
-                name: 'email',
-                message: "Please enter manager's email address.",
-                validate: titleInput => {
-                    if (titleInput) {
-                      return true;
-                    } else {
-                    console.log("Please enter manager's email address.");
-                    return false;
-                    }
-                    }
-                },
-                {
-                title: 'input',
-                name: 'office',
-                message: "Please enter manager's office number.",
-                validate: titleInput => {
-                    if (titleInput) {
-                      return true;
-                    } else {
-                    console.log("Please enter manager's office number.");
-                    return false;
-                    }
-                    }
-                },
-            ])
-            .then(managerInput => {
-                const {name, id, email, office} = managerInput
-                const manager = new managerInfo(name, id, email, office)
-
-                console.log(manager);
-            })
-        };
+function addManager() {
+    return managerInfo.addManager
+};
 
 function addEmployee() {
     return inquirer.prompt ([
@@ -88,18 +28,92 @@ function addEmployee() {
             return false;
             }
         }
-    }])
- 
+    },
+    {
+        title: 'input',
+        name: 'name',
+        message: "Please enter employee name.",
+        validate: titleInput => {
+            if (titleInput) {
+              return true;
+            } else {
+            console.log("Please enter employee name.");
+            return false;
+            }
+            }
+        },
+        {
+        title: 'input',
+        name: 'id',
+        message: "Please enter employee id number.",
+        validate: titleInput => {
+            if (titleInput) {
+              return true;
+            } else {
+            console.log("Please enter employee id number.");
+            return false;
+            }
+            }
+        },
+        {
+            title: 'input',
+            name: 'email',
+            message: "Please enter employee's email address.",
+            validate: titleInput => {
+                if (titleInput) {
+                  return true;
+                } else {
+                console.log("Please enter employee's email address.");
+                return false;
+                }
+                }
+            },
+            {
+            title: 'input',
+            name: 'username',
+            message: "Please enter employee's GitHub username.",
+            when: (input) => input.role === teamArr[1],
+            validate: titleInput => {
+                if (titleInput) {
+                  return true;
+                } else {
+                console.log("Please enter employee's username.");
+                return false;
+                }
+                }
+            },
+            {
+                title: 'input',
+                name: 'school',
+                message: "Please enter name of intern's school.",
+                when: (input) => input.role === teamArr[2],
+                validate: titleInput => {
+                    if (titleInput) {
+                      return true;
+                    } else {
+                    console.log("Please enter school name.");
+                    return false;
+                    }
+                    }
+                },
+                {
+                    type: 'confirm',
+                    name: 'confirm',
+                    message: 'Add more team members?',
+                    default: false
+                }
+        ])
+    };
 
+
+let role = "role"
+if (role === teamArr[1]) {
+    exports.engineer = this.engineer
+    console.log(engineer);
+} else if (role === teamArr[2]) {
+    exports.intern = this.intern
+    console.log(intern);
 }
-addEmployee(role)   
-if (role === teamArr[1]) (
-    employee = new engineerInfo()
-); else (
-    employee = new internInfo()
-);
-
-
 const writeFile = data => {
     fs.writeFile('./page/teamPage', data, err =>
     err ? console.log(err) : console.log('Team profiles created.'))
